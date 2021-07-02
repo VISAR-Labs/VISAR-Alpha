68ddb473910fe2b8228e632083e3b73655cab68d
FihlaTV <fihlatv@gmail.com>
2021-07-02 15:47:16 +0200

    Migrated to AndroidX Upgraded to latest SDK (SDK 29) Hardlinked lablinks back tp labs index Optimized Fragemnts Added chapters wallpaper

--- app/src/main/java/com/kumar/dipanshu/visar/Fragments/mechanics/XploreForceFragment.java
+++ app/src/main/java/com/kumar/dipanshu/visar/Fragments/mechanics/XploreForceFragment.java
@@ -35,7 +35,7 @@ public class XploreForceFragment extends Fragment {
         // Required empty public constructor
     }
 
-    String url = "https://labs.visar.co.za/forces-and-motion-basics/forces-and-motion-basics_en.html";
+    String url = "https://labs.visar.co.za";
     WebView wvPage1;
 
     @Override
